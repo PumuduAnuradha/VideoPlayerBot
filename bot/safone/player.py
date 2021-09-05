095f4546662eccc6f51ef26157d73e5973bf2546
PumuduAnuradha <89907103+PumuduAnuradha@users.noreply.github.com>
2021-09-05 12:26:21 +0530

    Update player.py

--- bot/safone/player.py
+++ bot/safone/player.py
@@ -176,11 +176,8 @@ allcmd = ["start", "help", f"start@{USERNAME}", f"help@{USERNAME}"] + admincmds
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
-            ],
-            [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/VideoPlayerBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/TheKumikoSetsuko"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/KumikoSetsuko"),
             ]
          ]
-    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group 🤷‍♂️! But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/VideoPlayerBot) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group 🤷‍♂️! **", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
